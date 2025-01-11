97a0fa5c9223fcdc163a3e61ab05e8d530ababe0
Ariel <arielacobos@hotmail.com>
2025-01-11 07:53:15 -0300

    Feat: Avatars List Component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,36 @@
-import { CardComponent, Header } from '@/components'
+import { AvatarsList, CardComponent, Header } from '@/components'
 import { Container } from '@mui/material'
+import { currencyConverter } from '@/utils'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(1234.54),
+    },
+
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(5634.0),
+    },
+
+    {
+      avatar: '/dnc-avatar.svg',
+      name: 'Nome Sobrenome 3',
+      subtitle: currencyConverter(4524.55),
+    },
+  ]
+
   return (
     <>
       <Header />
       <Container maxWidth="lg">
         <CardComponent className="">CARD</CardComponent>
+        <CardComponent>
+          <AvatarsList listData={mockListData} />
+        </CardComponent>
       </Container>
     </>
   )
