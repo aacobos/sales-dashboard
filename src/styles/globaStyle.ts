82e2dafe9ca6f563cfac4e016d2a05ee9ea5e1ab
Ariel <arielacobos@hotmail.com>
2025-01-20 07:11:27 -0300

    Feat: Logout Flow

--- src/styles/globaStyle.ts
+++ src/styles/globaStyle.ts
@@ -1,17 +1,26 @@
 import { createGlobalStyle } from 'styled-components'
 import { DefaultTheme } from 'styled-components'
+import { pxToRem } from '@/utils'
 
 export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
     body, html {
-    background: ${(props) => props.theme.appBackground};
-    color: ${(props) => props.theme.appColor};
-    margin: 0;
-    padding: 0;
-    font-family: "Inter", sans-serif;
+        background: ${(props) => props.theme.appBackground};
+        color: ${(props) => props.theme.appColor};
+        margin: 0;
+        padding: 0;
+        font-family: "Inter", sans-serif;
     }
 
     h1, h2, p, ul, li, figure {
-    margin: 0;
-    padding: 0;
+        margin: 0;
+        padding: 0;
+    }
+
+    .mb-1 {
+        margin-bottom: ${pxToRem(16)};
+    }
+
+    .mb-2 {
+        margin-bottom: ${pxToRem(32)};
     }
 `
